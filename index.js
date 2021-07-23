b660524482f20d1c906550e19faff7e892fea19d
minh <minhbh2000@gmail.com>
2021-07-23 21:13:02 +0700

    abc

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function number(text1) {
+     text1 = document.getElementById("text").Value;
+    if (text1 == "6") {
+        alert("Chúc mừng bạn đã chọn đúng");
+    }else {
+        alert("Bạn nhập sai hãy nhập lại!!!")
+    }
+}
